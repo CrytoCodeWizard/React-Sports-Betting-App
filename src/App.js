6b1c4e52643cff5b32c047b2f74af606e1bf36c9
David Teo <starrynightskyhoney@gmail.com>
2023-01-04 00:00:17 -0500

    State remains selected on refresh

--- src/App.js
+++ src/App.js
@@ -2,133 +2,133 @@ import React,{ useEffect, useState } from "react";
 import GameOverview from "./Components/GameOverview";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import Select from "react-dropdown-select";
-
-function App() {
-
-  const [games, setGames] = useState([]);
-  const [sport, setSport] = useState(window.localStorage.getItem('sport') || 'americanfootball_nfl');
-  const [bookies, setBookies] = useState(new Set([]));
+import Select from "react-select";
 
   const state_bookmakers = [
     { 
       bookmakers: new Set(["barstool", "betmgm", "betrivers", "draftkings", "fanduel", "sugarhouse", "superbook", "twinspires", "unibet_us", "williamhill_us", "wynnbet"]),
-      value: 1,
+      value: 0,
       label: "Arizona"
     },
     {
       bookmakers: new Set(["barstool", "betmgm", "betrivers", "circasports", "draftkings", "fanduel", "foxbet", "pointsbetus", "sugarhouse", "superbook", "williamhill_us", "wynnbet"]),
-      value: 2,
+      value: 1,
       label: "Colorado"
     },
     {
       bookmakers: new Set(["betrivers", "draftkings", "fanduel", "sugarhouse"]),
-      value: 3,
+      value: 2,
       label: "Connecticut"
     },
     {
       bookmakers: new Set(["barstool", "betmgm", "betrivers", "draftkings", "fanduel", "pointsbetus", "sugarhouse", "williamhill_us"]),
-      value: 4,
+      value: 3,
       label: "Illinois"
     },
     {
       bookmakers: new Set(["barstool", "betmgm", "betrivers", "draftkings", "fanduel", "pointsbetus", "sugarhouse", "unibet_us", "williamhill_us", "wynnbet"]),
-      value: 5,
+      value: 4,
       label: "Indiana"
     },
     {
       bookmakers: new Set(["barstool", "betmgm", "betrivers", "circasports", "draftkings", "fanduel", "pointsbetus", "sugarhouse", "superbook", "unibet_us", "williamhill_us"]),
-      value: 6,
+      value: 5,
       label: "Iowa"
     },
     {
       bookmakers: new Set(["barstool", "betmgm", "draftkings", "fanduel", "pointsbetus", "williamhill_us"]),
-      value: 7,
+      value: 6,
       label: "Kansas"
     },
     {
       bookmakers: new Set(["barstool", "betmgm", "betrivers", "draftkings", "fanduel", "pointsbetus", "sugarhouse", "williamhill_us", "wynnbet"]),
-      value: 8,
+      value: 7,
       label: "Lousiana"
     },
     {
       bookmakers: new Set(["barstool", "betmgm", "betrivers", "draftkings", "fanduel", "pointsbetus", "sugarhouse", "williamhill_us"]),
-      value: 9,
+      value: 8,
       label: "Maryland"
     },
     {
       bookmakers: new Set(["barstool", "betmgm", "betrivers", "draftkings", "fanduel", "foxbet", "pointsbetus", "sugarhouse", "williamhill_us", "wynnbet"]),
-      value: 10,
+      value: 9,
       label: "Michigan"
     },
     {
       bookmakers: new Set(["betmgm"]),
-      value: 11,
+      value: 10,
       label: "Mississippi"
     },
     {
       bookmakers: new Set(["circasports", "superbook", "williamhill_us"]),
-      value: 12,
+      value: 11,
       label: "Nevada"
     },
     {
       bookmakers: new Set(["draftkings"]),
-      value: 13,
+      value: 12,
       label: "New Hampshire"
     },
     {
       bookmakers: new Set(["barstool", "betfair", "betmgm", "betrivers", "draftkings", "fanduel", "foxbet", "pointsbetus", "sugarhouse", "superbook", "unibet_us", "williamhill_us", "wynnbet"]),
-      value: 14,
+      value: 13,
       label: "New Jersey"
     },
     {
       bookmakers: new Set(["betmgm", "betrivers", "draftkings", "fanduel", "pointsbetus", "sugarhouse", "williamhill_us", "wynnbet"]),
-      value: 15,
+      value: 14,
       label: "New York"
     },
     {
       bookmakers: new Set(["betmgm", "betrivers", "draftkings", "fanduel", "pointsbetus", "sugarhouse"]),
-      value: 16,
+      value: 15,
       label: "Ohio"
     },
     {
       bookmakers: new Set(["draftkings"]),
-      value: 17,
+      value: 16,
       label: "Oregon"
     },
     {
       bookmakers: new Set(["barstool", "betmgm", "betrivers", "draftkings", "fanduel", "foxbet", "pointsbetus", "sugarhouse", "twinspires", "unibet_us", "williamhill_us"]),
-      value: 18,
+      value: 17,
       label: "Pennsylvania"
     },
     {
       bookmakers: new Set(["barstool", "betmgm", "draftkings", "fanduel", "superbook", "williamhill_us", "wynnbet"]),
-      value: 19,
+      value: 18,
       label: "Tennessee"
     },
     {
       bookmakers: new Set(["barstool", "betmgm", "betrivers", "draftkings", "fanduel", "pointsbetus", "sugarhouse", "unibet_us", "williamhill_us", "wynnbet"]),
-      value: 20,
+      value: 19,
       label: "Virginia"
     },
     {
       bookmakers: new Set(["betmgm", "williamhill_us"]),
-      value: 21,
+      value: 20,
       label: "Washington DC"
     },
     {
       bookmakers: new Set(["barstool", "betmgm", "betrivers", "draftkings", "fanduel", "pointsbetus", "sugarhouse", "williamhill_us"]),
-      value: 22,
+      value: 21,
       label: "West Virginia"
     },
     {
       bookmakers: new Set(["betmgm", "draftkings", "fanduel", "williamhill_us"]),
-      value: 23,
+      value: 22,
       label: "Wyoming"
     },
     
   ]
 
+function App() {
+  
+  const [games, setGames] = useState([]);
+  const [sport, setSport] = useState(window.localStorage.getItem('sport') || 'americanfootball_nfl');
+  const [bookies, setBookies] = useState(window.localStorage.getItem('usState')?state_bookmakers[window.localStorage.getItem('usState')]["bookmakers"]:new Set([])) ;
+  
   useEffect(() => {
     const urls = ['https://odds.p.rapidapi.com/v4/sports/' + sport + '/odds?regions=us&oddsFormat=decimal&markets=spreads&dateFormat=iso', 'https://odds.p.rapidapi.com/v4/sports/' + sport + '/scores'];
     Promise.all(urls.map(url => fetch(url, {
@@ -148,18 +148,22 @@ function App() {
         });
         window.localStorage.setItem('sport', sport);
     }, [sport, bookies]);
+  
+  function stateSelect(values){
+    setBookies(values["bookmakers"]);
+    window.localStorage.setItem('usState', values["value"]);
+  }
 
   return (
     <div>
       <div className="title-heading">Live Sporting Odds</div>
-      <p></p>
         <nav className="navbar navbar-light navbar-custom justify-content-center">    
           {sport === 'americanfootball_nfl'?<button className="active navbar-text-custom nav-button-selected" onClick={() => setSport('americanfootball_nfl')}>NFL</button>:
           <button className="active navbar-text-custom nav-button" onClick={() => setSport('americanfootball_nfl')}>NFL</button>}
           {sport === 'basketball_nba'?<button className="active navbar-text-custom nav-button-selected" onClick={() => setSport('basketball_nba')}>NBA</button>:
           <button className="active navbar-text-custom nav-button" onClick={() => setSport('basketball_nba')}>NBA</button>}
         </nav>
-        <Select options={state_bookmakers} onChange={(values) => setBookies(values[0].bookmakers)} />
+        <div className="state-dropdown"><Select options={state_bookmakers} style={{width: "175px"}} defaultValue={state_bookmakers[window.localStorage.getItem('usState')] || ""} onChange={(values) => stateSelect(values)} /></div>
       <div className="app-container">
         <div className="game-container">
           <div className="all-container">
