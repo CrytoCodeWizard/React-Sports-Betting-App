3edf8c05d02f7846255e0cefa72a86d7e817c98e
David Teo <starrynightskyhoney@gmail.com>
2023-08-12 12:32:02 -0400

    make maximun number of pages 3, only show player props button within 2 days or less

--- src/Components/GameOverview.js
+++ src/Components/GameOverview.js
@@ -16,9 +16,10 @@ const GameOverview = (game) => {
     const [showTeamProps,setShowTeamProps] = useState(window.sessionStorage.getItem('team_prop_clicked_' + game.game_id) === 'true' ? true : false);
     const [showPlayerProps, setShowPlayerProps] = useState(window.sessionStorage.getItem('player_prop_clicked_' + game.game_id) === 'true' ? true : false);
     const [playerPropsClicked, setPlayerPropsClicked] = useState(window.sessionStorage.getItem('player_prop_clicked_' + game.game_id) === 'true' ? true : false);
-    const today = process.env.JEST_WORKER_ID ? new Date('2023-08-06T00:20:00Z') : new Date();
+    const today = process.env.JEST_WORKER_ID ? new Date('2023-09-06T00:20:00Z') : new Date();
     const gameStart = new Date(game.startTime);
     const isLive = today>=gameStart;
+    const daysTilStart = (gameStart - today) / (1000 * 3600 * 24)
     const stringifiedGameStart = gameStart.toLocaleString([], {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit'});
 
     return (
@@ -54,9 +55,7 @@ const GameOverview = (game) => {
                     {game.curScore?<span>{game.curScore[1].score} - {game.curScore[0].score}</span>:<></>}
                     </Typography>
                 </div>
-                
-
-                {!isLive?
+                {!isLive && daysTilStart <= 2 ?
                 <div className="h-24 w-48 mx-auto flex justify-center items-center">
                     {showTeamProps===true?<Button variant="text" className="w-1/2 border-r-2 font-bold" color="blue" onClick={() => teamPress()}>Team<br></br>Props</Button>
                     :<Button variant="text" className="w-1/2 border-r-2" color="blue-gray" onClick={() => teamPress()}>Team<br></br>Props</Button>}
